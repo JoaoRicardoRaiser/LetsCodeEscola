b1e6b1aa24850cfa6e7708dd3d15c42712c57a71
ivo.junior <ivo.junior@ambevtech.com.br>
2022-03-29 15:16:45 -0300

    Chamda Repository

---
+++ LestsCodeEscola/LestsCodeEscola/Professor/Entidades/Chamada.cs
@@ -0,0 +1,13 @@
+﻿using LestsCodeEscola.Comum;
+using System.Collections.Generic;
+
+namespace LestsCodeEscola.Professor.Entidades
+{
+    public class Chamada : EntidadeBase
+    {
+        public string Turma { get; set; }
+        public List<string> alunos;
+        public bool Finalizada { get; set; } = false;
+        public string Data { get; set; }
+    }
+}
---
+++ LestsCodeEscola/LestsCodeEscola/Professor/Repository/ChamadaRepository.cs
@@ -0,0 +1,15 @@
+﻿using AtividadeScrumLetsCode.Repositories;
+using LestsCodeEscola.Professor.Entidades;
+using System.IO;
+using System.Linq;
+
+namespace LestsCodeEscola.Professor.Repository
+{
+    public class ChamadaRepository : GenericRepository<Chamada>
+    {
+        public ChamadaRepository()
+        {
+            Host = Directory.GetCurrentDirectory() + @"..\..\..\..\professor\Database\chamada.json";
+        }
+    }
+}
